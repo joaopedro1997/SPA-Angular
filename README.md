8a6cf78244773602d19616f6efcd27de28042436
joaopedro1997 <joaopedrodiasamaral@gmail.com>
2020-02-02 19:18:36 -0300

    atualizando readme.md

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ O aplicativo será recarregado automaticamente se você alterar qualquer um dos 
 ##Scenes
 
 -[x] Criar o primeiro componente.
+-[x] Criar um componente usando o angular cli.
+-[x] Baixar e usar o bootstrap no projeto.
+
+
+## Comands
+ * `ng generate component [nome-do-componente]` ou `ng g c [nome-do-componente]` // cria um componente angular. 
+ * `npm i bootstrap` // baixa o bootstrap para o nosso projeto via npm
+ * Vá até o arquivo angular.json na raíz do projeto, vá até styles e acrecente o caminho do bootstrap. ` "styles": [
+                 "src/styles.css",
+                 "node_modules/bootstrap/dist/css/bootstrap.min.css"
+               ],` 
+* 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {OlaMundoComponent} from './ola-mundo/ola-mundo.component';
 
+import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 @NgModule({
   declarations: [
     AppComponent,
-    OlaMundoComponent
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
   ],
   imports: [
     BrowserModule
